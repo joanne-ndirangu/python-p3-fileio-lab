6ee396bc7000f989cf638177c1fb6640b5a3da01
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-09-09 21:50:11 +0300

    Completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,10 @@
 def write_file(file_name, file_content):
-    pass
-
+    with open(f'{file_name}.txt', mode='w') as text_file:
+        text_file.write(file_content)
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name="test_file"):
+    with open(f'{file_name}.txt') as f:
+        return f.read()
